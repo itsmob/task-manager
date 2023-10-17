599f70744932aeba71a67258f18c3e9b774ad74a
itsmob <mobster.dev.96@gmail.com>
2023-10-17 09:54:45 -0400

    Change port of the server to the enviroment process port

--- server/src/config.js
+++ server/src/config.js
@@ -5,3 +5,5 @@ config();
 export const TOKEN_SECRECT = process.env.TOKEN_SECRECT;
 
 export const MONGO_DB_URI = process.env.DB_URI || 'mongodb://127.0.0.1:27017/backupdb';
+
+export const PORT = process.env.PORT || 4000;
